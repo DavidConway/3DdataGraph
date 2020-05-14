4e19052422d9b22378cd8c8c64a387fd18032fbf
DavidConway <dconway099@gmail.com>
2020-05-14 20:35:39 +0100

    first commit
    .

---
+++ Assets/3DDataGraph/BarChart/highestBarValue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace barChart
+{
+    public class highestBarValue
+    {
+        public highestBarValue(string[,] data, string[] cats)
+        {
+            Dictionary<string, int> dataPoss = new Dictionary<string, int>();
+            for(int i = 0; i < data.GetLength(0); i++)
+            {
+                string test = "";
+                for (int j =0; j < data.GetLength(1); j++)
+                {
+                    test += data[i, j];
+                }
+
+                if (dataPoss.TryGetValue(test, out int k) == true)
+                {
+                    dataPoss[test] = dataPoss[test]+1;
+                }
+                else
+                {
+                    dataPoss.Add(test, 1);
+                }
+            }
+            int test2 = 0;
+        }
+    }
+}
---
+++ Assets/3DDataGraph/dataGraph.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DataParsing;
+using barChart;
+
+namespace DataGraph
+{
+    public class dataGraph : MonoBehaviour
+    {
+        private dataPars parser;
+        private barChartMain barChart;
+        public enum dataScorce { googleSheet }; //possible dataScorses
+        public enum graphType { barchart };
+        private graphType mode;
+        private int[] startingPoint = new int[2]; // two ints to represent the data point 
+        private int page;
+        
+
+        void Start()
+        {
+            mode = graphType.barchart;
+            startingPoint[0] = 0;
+            startingPoint[1] = 0;
+            page = 2;
+
+            //calls parser
+            parser = new dataPars(dataScorce.googleSheet, "https://docs.google.com/spreadsheets/d/1DF15HgrXx9X-ntNgUI0RJhoUhc548RB855a-UvSj_kw/edit#gid=0",page-1,startingPoint);
+
+            //sets mode
+            switch (mode)
+            {
+                case graphType.barchart:
+                    {
+                        barChart = new barChartMain(parser.getParsedData(), parser.getCatagorys()); ;
+                        break;
+                    }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    }
+}
