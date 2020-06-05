01c548ead335cffbdeb29114d0c58fb677277607
DavidConway <dconway099@gmail.com>
2020-06-05 19:51:19 +0100

    added function to get all posibel option for a width and lenght, as well and the number of options

--- Assets/3DDataGraph/BarChart/barChartMain.cs
+++ Assets/3DDataGraph/BarChart/barChartMain.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 namespace barChart
@@ -7,11 +9,17 @@ namespace barChart
     public class barChartMain
     {
         string[,] data;
+        int arrayWidth;
+        int arrayLenght;
+        List<string> widthElemets = new List<string>();
+        List<string> lenghtElemets = new List<string>();
         public barChartMain(object[,] dataIn, string[] cats)
         {
             data = dataConvert(dataIn);
             groupValues values = new groupValues(data, cats);
-            Debug.Log(values.ToString());
+            int arrayWidth = getWidth();// generate width info and lenght
+            int arrayLenght = getLenght();//get lenght info and lenght.... you know what i mean
+            test();
         }
 
         private string[,] dataConvert(object[,] data)//changes all data to strings
@@ -26,6 +34,50 @@ namespace barChart
             }
 
             return output;
+        }
+
+        private int getWidth() // gose trow all width elements if it hasent been counted yet its added to the list and the number of elements increses i.e (red,blue,green)
+        {
+            int outNum = 0;
+            for(int i = 0; i < data.GetLength(0); i++)
+            {
+                if (!widthElemets.Contains(data[i, 0]))
+                {
+                    widthElemets.Add(data[i, 0]);
+                    outNum++;
+                }
+            }
+            return outNum;
+        }
+
+        private int getLenght() // gose trow all lenght elements if it hasent been counted yet its added to the list and the number of elements increses i.e (car,van,truck)
+        {
+            int outNum = 0;
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                if (!lenghtElemets.Contains(data[i, 1]))
+                {
+                    lenghtElemets.Add(data[i, 1]);
+                    outNum++;
+                }
+            }
+            return outNum;
+        }
+
+        void test()
+        {
+            string width = "";
+            foreach(string i in widthElemets)
+            {
+                width += i + " ";
+            }
+            string lenght = "\n";
+            foreach(string i in lenghtElemets)
+            {
+                lenght += i + " ";
+            }
+            Debug.Log(width);
+            Debug.Log(lenght);
         }
     }
 }
