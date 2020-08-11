f43eb8ef199c9cbb1ff8d9d5e6d2b5394cf18f9d
DavidConway <dconway099@gmail.com>
2020-08-11 20:36:41 +0100

    data line names
    added text spawning for the data, and set up text auto scaling

--- Assets/3DDataGraph/BarChart/barChartMain.cs
+++ Assets/3DDataGraph/BarChart/barChartMain.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Animations;
 
 namespace barChart
 {
@@ -15,28 +18,20 @@ namespace barChart
         List<string> widthElemets = new List<string>();
         List<string> lenghtElemets = new List<string>();
         GameObject graphObj;
+        groupValues values;
         public barChartMain(object[,] dataIn, GameObject graphObjIn)
         {
             graphObj = graphObjIn;
             data = dataConvert(dataIn);
-            groupValues values = new groupValues(data);// generates a directory of string(row, col names) to int the number of times it happens
+            values = new groupValues(data);// generates a directory of string(row, col names) to int the number of times it happens
 
             biggestGroup = values.highestValue;
             int arrayWidth = getWidth();// generate width info and lenght
             int arrayLenght = getLenght();//get lenght info and lenght.... you know what i mean
 
-            for(int i = 0; i < widthElemets.Count; i++) //iterates trow and spawns
-            {
-                for(int j = 0; j< lenghtElemets.Count; j++)
-                {
-                    string check = widthElemets[i] + lenghtElemets[j];
-                    int numofInst;
-                    if (values.dataPoss.TryGetValue(check, out numofInst))
-                    {
-                        dataBar.spawn(graphObj, i, numofInst, j, check);
-                    }
-                }
-            }
+            spawnArray();
+            generateNames();
+
         }
 
         private string[,] dataConvert(object[,] data)//changes all data to strings
@@ -95,6 +90,81 @@ namespace barChart
             }
             Debug.Log(width);
             Debug.Log(lenght);
+        }
+
+        private void spawnArray()
+        {
+            GameObject bars = new GameObject();
+            bars.name = "data";
+            bars.transform.parent = graphObj.transform;//creates object to place bars in
+            bars.transform.localPosition = new Vector3(0, 0, 0);
+
+            for (int i = 0; i < widthElemets.Count; i++) //iterates trow and spawns
+            {
+                for (int j = 0; j < lenghtElemets.Count; j++)
+                {
+                    string check = widthElemets[i] + lenghtElemets[j];
+                    int numofInst;
+                    if (values.dataPoss.TryGetValue(check, out numofInst))
+                    {
+                        dataBar.spawn(bars, i, numofInst, j, check);
+                    }
+                }
+            }
+        }
+
+        private void generateNames()
+        {
+            GameObject dataText = new GameObject();
+            dataText.name = "dataText";
+            dataText.transform.parent = graphObj.transform;//creates object to place bars in
+            dataText.transform.localPosition = new Vector3(0, 0, 0);
+
+            foreach (string i in widthElemets)
+            {
+                TextMeshPro text;
+                GameObject newText = new GameObject(i);
+                newText.AddComponent<TextMeshPro>();
+                text = newText.GetComponent<TextMeshPro>();
+                RectTransform rect = newText.GetComponent<RectTransform>();
+
+                text.text = i;
+                text.enableAutoSizing = true;
+                text.fontSizeMin = 1;
+                text.fontSizeMax = 5;
+
+                text.alignment = TextAlignmentOptions.Center;// text setup
+                rect.sizeDelta = new Vector2(1.5f, 1);
+
+                int x = widthElemets.IndexOf(i);
+                newText.transform.parent = dataText.transform;
+                newText.transform.localPosition = new Vector3((float)(x + (0.5 * x)), 0.5f, -1);//text alinement
+
+            }
+
+            foreach (string i in lenghtElemets)
+            {
+                TextMeshPro text;
+                GameObject newText = new GameObject(i);
+                newText.AddComponent<TextMeshPro>();
+                text = newText.GetComponent<TextMeshPro>();
+                RectTransform rect = newText.GetComponent<RectTransform>();
+
+                text.text = i;
+                text.enableAutoSizing = true;
+                text.fontSizeMin = 1;
+                text.fontSizeMax = 5;
+
+                text.alignment = TextAlignmentOptions.Center;// text setup
+                rect.sizeDelta = new Vector2(1.5f, 1);
+
+                int x = lenghtElemets.IndexOf(i);
+                newText.transform.parent = dataText.transform;
+                newText.transform.Rotate(new Vector3(0, 90, 0), Space.Self);
+                newText.transform.localPosition = new Vector3(-1, 0.5f, (float)(x + (0.5 * x)));//text alinement
+            }
+
+
         }
     }
 }
